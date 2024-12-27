ebd760c962a9f3c3771b3124926dcc69e94cdbbf
Aaditya Narain <aaditya.narain.avna@gmail.com>
2024-12-27 23:09:25 +0530

    feat: added models, db connection, no schema required

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,4 +1,16 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
-db = client["conference"]
+MONGO_URI = "mongodb://localhost:27017"
+DATABASE_NAME = "conference"
+
+client = MongoClient(MONGO_URI)
+db = client[DATABASE_NAME]  
+
+collections = ["users", "submissions", "research_tracks", "announcements", "schedule", "registrations"]
+for collection in collections:
+    if collection not in db.list_collection_names():
+        db.create_collection(collection)
+
+    
+def get_db():
+    return db
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from app.routes import auth
+#from app.routes import auth
+from db import get_db
 
 app = FastAPI(root_path="/api")
 
-app.include_router(auth.router, prefix="/auth")
+#app.include_router(auth.router, prefix="/auth")
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to IIITH Conference API"}
+    db=get_db()
+    #return {"message": "Welcome to IIITH Conference API"}
+    return str(db.client)
