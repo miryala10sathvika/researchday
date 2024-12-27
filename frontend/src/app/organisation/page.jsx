d297d1caa9f60a350459815205cd26602c38ff22
dileepadari <adaridileep@gmail.com>
2024-12-27 15:16:08 +0530

    updated homepage

---
+++ frontend/src/app/organisation/page.jsx
@@ -0,0 +1,8 @@
+export default function Organisation() {
+    return (
+        <div>
+            <h1>Organisation</h1>
+            <p>This is the organisation page.</p>
+        </div>
+    );
+}
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,6 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-
 const logo = "/assets/logo.jpg";
 
 export default function Header() {
