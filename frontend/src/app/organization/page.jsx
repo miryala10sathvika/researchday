08e84dbbcfefc59ce52ec7be2d99fdeeaf2817f4
dileepadari <adaridileep@gmail.com>
2024-12-30 02:00:23 +0530

    organization page revised, menu options highlighted

---
+++ frontend/src/app/organization/page.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import Card from 'components/MemberCard';
+import { Box, Typography } from '@mui/material';
+
+const organizers = [
+  {
+    position: 'Program Committee Chair',
+    members: [{ name: 'Arnab Bhattacharya', institute: 'IIT Kanpur' }],
+  },
+  {
+    position: 'Organizing Chair',
+    members: [{ name: 'Martin Gluckman', institute: 'Sanskrit Research Institute' }],
+  },
+  {
+    position: 'Web Chair',
+    members: [{ name: 'Hrishikesh Terdalkar', institute: 'IIT Kanpur, Kanpur, India' }],
+  },
+  {
+    position: 'Steering Committee',
+    members: [
+      { name: 'Amba Kulkarni', institute: 'University of Hyderabad, Hyderabad, India' },
+      { name: 'Arnab Bhattacharya', institute: 'IIT Kanpur, Kanpur, India' },
+      { name: 'Brendan Gillon', institute: 'McGill University, Montreal, Canada' },
+      { name: 'Gérard Huet', institute: 'INRIA, Paris, France' },
+      { name: 'Malhar Kulkarni', institute: 'IIT Bombay, Mumbai, India' },
+      { name: 'Pawan Goyal', institute: 'IIT Kharagpur, Kharagpur, India' },
+      { name: 'Peter Scharf', institute: 'The Sanskrit Library, USA' },
+    ],
+  },
+  {
+    position: 'Technical Program Committee',
+    members: [
+      { name: 'Amba Kulkarni', institute: 'University of Hyderabad, Hyderabad, India' },
+      { name: 'Amrith Krishna', institute: 'Manipal Academy of Higher Education, Bengaluru, India' },
+      { name: 'Arjuna S R', institute: 'Manipal Academy of Higher Education, Bengaluru, India' },
+      { name: 'Arnab Bhattacharya', institute: 'IIT Kanpur, Kanpur, India' },
+      { name: 'Brendan Gillon', institute: 'McGill University, Montreal, Canada' },
+      { name: 'Chaitali Dangarikar', institute: 'IIT Kanpur, Kanpur, India' },
+      { name: 'Ganesh Ramakrishnan', institute: 'IIT Bombay, Mumbai, India' },
+      { name: 'Gérard Huet', institute: 'INRIA, Paris, France' },
+      { name: 'Malhar Kulkarni', institute: 'IIT Bombay, Mumbai, India' },
+      { name: 'Oliver Hellwig', institute: 'University of Zurich, Zurich, Switzerland' },
+      { name: 'Patrick McAllister', institute: 'Austrian Academy of Sciences, Austria' },
+      { name: 'Pavankumar Satuluri', institute: 'Indian Institute of Technology Roorkee, Roorkee, India' },
+      { name: 'Pawan Goyal', institute: 'IIT Kharagpur, Kharagpur, India' },
+      { name: 'Peter Scharf', institute: 'The Sanskrit Library, USA' },
+      { name: 'Sebastian Nehrdich', institute: 'University of Hamburg, Hamburg, Germany' },
+      { name: 'Shivani V', institute: 'Karnataka Sanskrit University, Karnataka, India' },
+      { name: 'Tanuja P Ajotikar', institute: 'The Sanskrit Library, USA' },
+    ],
+  },
+];
+
+export default function Organization() {
+  return (
+    <Box sx={{ display: 'flex',flexDirection: { xs: 'column', md: 'row' }, gap: '20px', padding: '20px', ml: { xs: 0, lg: 25 }, mr: { xs: 0, lg: 25 }, mt:15 }}>
+      {/* First Column */}
+      <Box
+        sx={{
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '20px',
+        }}
+      >
+        {organizers
+          .slice(0, 4)
+          .map((item, index) => (
+            <Card key={index} position={item.position} members={item.members} />
+          ))}
+      </Box>
+
+      {/* Second Column */}
+      <Box
+        sx={{
+          flex: 1,
+          display: 'grid',
+          gridTemplateColumns: '1fr',
+          gap: '20px',
+        }}
+      >
+        {organizers
+        .slice(4, 5)
+        .map((item, index) => (
+            <Card key={index} position={item.position} members={item.members} />
+          ))}
+      </Box>
+    </Box>
+  );
+}
---
+++ frontend/src/components/MemberCard.jsx
@@ -0,0 +1,64 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+export default function MemberCard({ position, members }) {
+  return (
+    <Box
+      sx={{
+        border: '1px solid #e0e0e0',
+        borderRadius: '10px',
+        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+        bgcolor: '#ffffff',
+        overflow: 'hidden',
+        display: 'flex',
+        flexDirection: 'column',
+        height: 'max-content',
+      }}
+    >
+      <Box
+        bgcolor="var(--theme-bg-color)"
+        color="#ffffff"
+        p={2}
+        fontSize="1.2em"
+        fontWeight="bold"
+        textAlign="center"
+        textTransform="uppercase"
+      >
+        {position}
+      </Box>
+      <Box p={2}>
+        {members.map((person, index) => (
+          <React.Fragment key={index}>
+            {index > 0 && (
+              <Box
+                component="hr"
+                border="none"
+                borderTop="1px solid #e0e0e0"
+                my={1}
+              />
+            )}
+            <Box>
+              <Box
+                component="p"
+                color="#333333"
+                fontSize="1em"
+                fontWeight="600"
+                mb={1}
+              >
+                {person.name}
+              </Box>
+              <Box
+                component="p"
+                color="#666666"
+                fontSize="0.9em"
+                textAlign={"left"}
+              >
+                {person.institute}
+              </Box>
+            </Box>
+          </React.Fragment>
+        ))}
+      </Box>
+    </Box>
+  );
+}
