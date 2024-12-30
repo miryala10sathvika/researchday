1ebb26a0fd857d5bad94633a84877b182acba640
dileepadari <adaridileep@gmail.com>
2024-12-30 11:59:51 +0530

    removed organisation folder

--- frontend/src/app/organisation/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Organisation() {
-    return (
-        <div>
-            <h1>Organisation</h1>
-            <p>This is the organisation page.</p>
-        </div>
-    );
-}
