dda49f1348ec1f152eeecbcac6c2bd5ce31d6153
Bhav Beri <bhavberi@gmail.com>
2025-01-23 22:23:28 +0530

    Updated batches of people

--- frontend/src/app/organization/page.jsx
+++ frontend/src/app/organization/page.jsx
@@ -8,22 +8,22 @@ const organizers = [
     members: [
       {
         name: "A Aparajitha",
-        batch: "PhD() 2k",
+        batch: "PhD(CL) 2k22",
         email: "aparajitha.allamraju@research.iiit.ac.in",
       },
       {
         name: "Furqan Shaik",
-        batch: "PhD() 2k",
+        batch: "PhD(CSE) 2k18",
         email: "furqan.shaik@research.iiit.ac.in",
       },
       {
         name: "Shodasakshari Vidya",
-        batch: "PhD() 2k",
+        batch: "PhD(CSE) 2k20",
         email: "shodasakshari.vidya@research.iiit.ac.in",
       },
       {
         name: "Sudarshan Srinivasan",
-        batch: "PhD() 2k",
+        batch: "PhD(CSE) 223k",
         email: "sudarshan.srinivasan@research.iiit.ac.in",
       },
     ],
@@ -51,12 +51,12 @@ const organizers = [
     position: "Organizing Committee",
     members: [
       { name: "Chandrasekar S", batch: "MS(CSE) 2k24" },
-      { name: "Kaveri Anuranjana", batch: "IIIT Hyderabad" },
-      { name: "Vedula Bhaskara Hanuma", batch: "IIIT Hyderabad" },
-      { name: "Janaksinh Ven", batch: "IIIT Hyderabad" },
-      { name: "Rasheed", batch: "IIIT Hyderabad" },
-      { name: "Lokesh V", batch: "IIIT Hyderabad" },
-      { name: "Aaryan Sharma", batch: "IIIT Hyderabad" },
+      { name: "Kaveri Anuranjana", batch: "PhD(CL) 2k23" },
+      { name: "Vedula Bhaskara Hanuma", batch: "PhD(CSE) 2k22" },
+      { name: "Janaksinh Ven", batch: "MS(ECE) 2k23" },
+      { name: "Rasheed", batch: "PhD(CSE) 2k22" },
+      { name: "Lokesh V", batch: "CSD UG2k20" },
+      { name: "Aaryan Sharma", batch: "LCD 2k22" },
     ],
   },
 ];
